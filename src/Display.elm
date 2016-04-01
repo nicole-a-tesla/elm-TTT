d4b2d81e58a72a5444317e5a72bcdc9956db94ec
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-04-01 11:52:23 -0400

    Add grid to display page

--- src/Display.elm
+++ src/Display.elm
@@ -1,12 +1,52 @@
 module Display (..) where
 
-import Html exposing (Html, div, h1)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 import Mouse
+
+cell : Attribute
+cell =
+  style
+    [("margin", "10px")
+    , ("width", "30%")
+    , ("height", "200px")
+    , ("text-align", "center")
+    , ("-webkit-user-select", "none")
+    , ("user-select", "none")
+    , ("font-size", "6em")
+    , ("border-radius", "0")
+    , ("-webkit-appearance", "none")
+    , ("padding", "0px")
+    , ("background", "white")
+    , ("border", "2px solid #4A4A4A"),
+      ("display", "inline-block")]
+
+header : Attribute
+header =
+  style
+    [("text-align", "center")]
+
+header : Attribute
+header =
+  style
+    [("text-align", "center")]
+
 
 
 view : Html
 view =
-  h1 [] [ Html.text "Welcome to Tic Tac Toe"]
+  div [] [
+    h1 [header] [ Html.text "Welcome to Tic Tac Toe"],
+    div [cell] [],
+    div [cell] [],
+    div [cell] [],
+    div [cell] [],
+    div [cell] [],
+    div [cell] [],
+    div [cell] [],
+    div [cell] [],
+    div [cell] []
+  ]
 
 main =
   view
