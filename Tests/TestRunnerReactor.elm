e7124a0981b8a88d237febf8e272c33395abd0ae
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-04-04 10:58:42 -0400

    Create seperate module to store test group to be imported to both testRunners

--- Tests/TestRunnerReactor.elm
+++ Tests/TestRunnerReactor.elm
@@ -1,16 +1,9 @@
 module Main (..) where
 
 import ElmTest exposing (..)
-import Tests
-
+import TestGroup
 
 main =
-  elementRunner
-    <| suite
-      "Tic Tac Toe Tests"
-      [ Tests.boardTests,
-        Tests.gameTests,
-        Tests.displayTest
-      ]
+  elementRunner TestGroup.tests
 
 
