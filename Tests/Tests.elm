80b324871e79088a6676561e70ad55cb85efe02c
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-04-05 11:41:08 -0400

    Clean up indentation on tests for readability.

--- Tests/Tests.elm
+++ Tests/Tests.elm
@@ -51,62 +51,62 @@ boardTests =
         "New Board is Empty"
         (assertEqual Board.newBoard [ [Empty, Empty, Empty], [Empty, Empty, Empty], [Empty, Empty, Empty]] )
     , test
-      "Return X for Human"
-      (assertEqual (Board.getSymbol Human) X)
+        "Return X for Human"
+        (assertEqual (Board.getSymbol Human) X)
     , test
-      "Return O for Computer"
-      (assertEqual (Board.getSymbol Computer) O)
+        "Return O for Computer"
+        (assertEqual (Board.getSymbol Computer) O)
     , test
-      "Extract item from list"
-      (assertEqual  (Just [1,2,3]) (Board.extractFromList [[1,2,3],[4,5,6]] 0))
+        "Extract item from list"
+        (assertEqual  (Just [1,2,3]) (Board.extractFromList [[1,2,3],[4,5,6]] 0))
     , test
-    "Extract another item from list"
-    (assertEqual (Just 1)(Board.extractFromList [1,2,3] 0))
+        "Extract another item from list"
+        (assertEqual (Just 1)(Board.extractFromList [1,2,3] 0))
     , test
-    "set nth item in list"
-    (assertEqual ([X, Empty])(Board.setNthItem [Empty, Empty] 0 X))
+        "set nth item in list"
+        (assertEqual ([X, Empty])(Board.setNthItem [Empty, Empty] 0 X))
     , test
-      "set nth item in list of lists"
-      (assertEqual ([[X, Empty], [Empty, Empty]])(Board.setNthItem [[Empty, Empty], [Empty, Empty]] 0 [X, Empty]))
+        "set nth item in list of lists"
+        (assertEqual ([[X, Empty], [Empty, Empty]])(Board.setNthItem [[Empty, Empty], [Empty, Empty]] 0 [X, Empty]))
     , test
-      "Get Row from 3x3 Board"
-      (assertEqual ([Empty, X, X])(Board.getRow test3x3Board 0))
+        "Get Row from 3x3 Board"
+        (assertEqual ([Empty, X, X])(Board.getRow test3x3Board 0))
     , test
-      "Get Column from 3x3 Board"
-      (assertEqual ([X, O, X])(Board.getColumn test3x3Board 1))
+        "Get Column from 3x3 Board"
+        (assertEqual ([X, O, X])(Board.getColumn test3x3Board 1))
     , test
-      "Get Diagonal from 3x3 Board"
-      (assertEqual ([Empty, O, Empty])(Board.getDiagonal test3x3Board))
+        "Get Diagonal from 3x3 Board"
+        (assertEqual ([Empty, O, Empty])(Board.getDiagonal test3x3Board))
     , test
-      "Get Anti-Diagonal from 3x3 Board"
-      (assertEqual ([X, O, Empty])(Board.getAntiDiagonal test3x3Board))
+        "Get Anti-Diagonal from 3x3 Board"
+        (assertEqual ([X, O, Empty])(Board.getAntiDiagonal test3x3Board))
     , test
-      "Get Row from 4x4 Board"
-      (assertEqual ([Empty, X, Empty, Empty])(Board.getRow test4x4Board 0))
+        "Get Row from 4x4 Board"
+        (assertEqual ([Empty, X, Empty, Empty])(Board.getRow test4x4Board 0))
     , test
-      "Get Column from 4x4 Board"
-      (assertEqual ([X, O, X, O])(Board.getColumn test4x4Board 1))
+        "Get Column from 4x4 Board"
+        (assertEqual ([X, O, X, O])(Board.getColumn test4x4Board 1))
     , test
-      "Get Diagonal from 4x4 Board"
-      (assertEqual ([Empty,O,Empty,Empty])(Board.getDiagonal test4x4Board))
+        "Get Diagonal from 4x4 Board"
+        (assertEqual ([Empty,O,Empty,Empty])(Board.getDiagonal test4x4Board))
     , test
-      "Get Anti-Diagonal from 4x4 Board"
-      (assertEqual ([Empty,Empty,X,Empty])(Board.getAntiDiagonal test4x4Board))
+        "Get Anti-Diagonal from 4x4 Board"
+        (assertEqual ([Empty,Empty,X,Empty])(Board.getAntiDiagonal test4x4Board))
     , test
-      "Update board with x in 1"
-      (assertEqual [[ X, Empty], [Empty, Empty]] (Board.update [[Empty, Empty],[Empty, Empty]] 0 0 Human))
+        "Update board with x in 1"
+        (assertEqual [[ X, Empty], [Empty, Empty]] (Board.update [[Empty, Empty],[Empty, Empty]] 0 0 Human))
     , test
-      "Check Row for X Winner"
-      (assertEqual "X Wins" (Board.checkListForWin [X, X, X]))
+        "Check Row for X Winner"
+        (assertEqual "X Wins" (Board.checkListForWin [X, X, X]))
     , test
-      "Check Row for O Winner"
-      (assertEqual "O Wins" (Board.checkListForWin [O, O, O]))
+        "Check Row for O Winner"
+        (assertEqual "O Wins" (Board.checkListForWin [O, O, O]))
     , test
-      "Check Row for No Winner"
-      (assertEqual "No Winner" (Board.checkListForWin [O, Empty, O]))
+        "Check Row for No Winner"
+        (assertEqual "No Winner" (Board.checkListForWin [O, Empty, O]))
     , test
-      "Check for 2 Unique Sybols"
-      (assertEqual 2 (Board.checkNumberOfUniqueSymbols [X, O, X]))
+        "Check for 2 Unique Sybols"
+        (assertEqual 2 (Board.checkNumberOfUniqueSymbols [X, O, X]))
 
     ]
 
