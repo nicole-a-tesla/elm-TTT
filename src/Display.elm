570c416f749b17ec7c1534a3b483395d30687f5a
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-06-01 15:05:09 -0400

    Add temporary 'Make Computer Move' button

--- src/Display.elm
+++ src/Display.elm
@@ -6,6 +6,7 @@ import Html.Events exposing (..)
 import Signal exposing (Signal, Address)
 
 import Board exposing (..)
+import Ai exposing (..)
 import DataTypes exposing (..)
 import Mailbox exposing (..)
 
@@ -20,6 +21,18 @@ cellStyle =
     , ("border-radius", "50")
     , ("background", "white")
     , ("border", "2px solid #4A4A4A")]
+
+computerMoveButtonStyle: Attribute
+computerMoveButtonStyle =
+    style
+    [("margin", "10px")
+    , ("width", "30%")
+    , ("height", "25px")
+    , ("font-size", "1em")
+    , ("border-radius", "50")
+    , ("background", "white")
+    , ("border", "2px solid #4A4A4A")]
+
 
 header : Attribute
 header =
@@ -38,19 +51,30 @@ buildWinnerString cell =
     Empty -> ""
     _ -> (toString cell) ++ " Wins"
 
+takeTurn: Int -> Action
+takeTurn index =
+    (Move (index//boardSize)(index % boardSize))
+
+takeComputerTurn: Action
+takeComputerTurn =
+  (ComputerMove)
+
 createCellButton : Int -> Cell -> Html
 createCellButton index cell =
   let
     content = convertCellToString index cell
   in
-    button [cellStyle, onClick actions.address (Move (index//boardSize)(index % boardSize))] [text content]
+  button [cellStyle, onClick actions.address (takeTurn index)] [text content]
 
-view : Address Action -> Game -> Html
+buildComputerMoveButton: GameState -> Html
+buildComputerMoveButton gameState =
+  button [computerMoveButtonStyle, onClick actions.address (takeComputerTurn)] [text "Make Computer Move"]
+
+view : Address Action -> GameState -> Html
 view address game =
   div [] [
     h1 [header] [ text "Welcome to Tic Tac Toe"],
+    (buildComputerMoveButton game),
     div [] (List.indexedMap createCellButton (List.concat game.board)),
     h1 [header] [text (buildWinnerString game.winner)]
   ]
-
-
